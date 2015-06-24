ce6d3e0f4ce197f183a2feef3dde714b0d951aea
Paul Huckaby <luap71@yahoo.com>
2015-06-23 19:59:06 -0700

    Update runme2_vagrant.sh
    clean up

--- runme2_vagrant.sh
+++ runme2_vagrant.sh
@@ -14,11 +14,11 @@ nova secgroup-add-rule sec1 icmp -1 -1 0.0.0.0/0
 for x in tcp udp; do nova secgroup-add-rule sec1 ${x} 1 65535 0.0.0.0/0 ; done
 
 neutron  net-create int
-neutron  subnet-create --gateway=2.0.0.254 --name=subint int 2.0.0.0/24 --enable-dhcp --dns-nameserver=192.168.111.254
+neutron  subnet-create --gateway=2.0.0.254 --name=subint int 2.0.0.0/24 --enable-dhcp --dns-nameserver=8.8.8.8
 neutron	 router-interface-add rtr1 subint
 
 neutron  net-create intTwo
-neutron  subnet-create --gateway=3.0.0.254 --name=subintTwo intTwo 3.0.0.0/24 --enable-dhcp --dns-nameserver=192.168.111.254
+neutron  subnet-create --gateway=3.0.0.254 --name=subintTwo intTwo 3.0.0.0/24 --enable-dhcp --dns-nameserver=8.8.8.8
 neutron	 router-interface-add rtr1 subintTwo
 
 # Tenant vm in subnet[One]
